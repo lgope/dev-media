9111a600220f7488a748acc52970b1625c73f5a8
lgope <ilakshmangope@gmail.com>
2020-05-23 20:55:56 +0600

    added firestore insert | get routes functionality 🔥

---
+++ dev-media-functions/functions/index.js
@@ -0,0 +1,54 @@
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+
+admin.initializeApp();
+
+const express = require('express');
+const app = express();
+
+// get data from firestore
+app.get('/screams', (req, res) => {
+  admin
+    .firestore()
+    .collection('screams')
+    .orderBy('createdAt', 'desc')
+    .get()
+    .then(data => {
+      let screams = [];
+      data.forEach(doc => {
+        screams.push({
+          screamId: doc.id,
+          body: doc.data().body,
+          userHandle: doc.data().userHandle,
+          createdAt: doc.data().createdAt,
+        });
+      });
+      return res.json(screams);
+    })
+    .catch(err => console.error(err));
+});
+
+// create new data and add to firestore
+app.post('/scream', (req, res) => {
+  const newScream = {
+    body: req.body.body,
+    userHandle: req.body.userHandle,
+    createdAt: new Date().toISOString(),
+  };
+
+  admin
+    .firestore()
+    .collection('screams')
+    .add(newScream)
+    .then(doc => {
+      res.json({ message: `document ${doc.id} created successfully` });
+    })
+    .catch(err => {
+      res.status(500).json({ error: 'Something went wrong!' });
+      console.error(err);
+    });
+});
+
+// https://baseurl.com/api/
+
+exports.api = functions.https.onRequest(app);
