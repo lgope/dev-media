6ba350c1df7444b68c8eaaa024c3751cef67163d
lgope <ilakshmangope@gmail.com>
2020-05-28 22:50:13 +0600

    fatch one scream | comment routes 🚀

--- dev-media-functions/functions/handlers/screams.js
+++ dev-media-functions/functions/handlers/screams.js
@@ -47,3 +47,67 @@ exports.postOneScream = (req, res) => {
       console.error(err);
     });
 };
+
+// get one scream by Id
+exports.getScream = (req, res) => {
+  let screamData = {};
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Scream not found' });
+      }
+      screamData = doc.data();
+      screamData.screamId = doc.id;
+      return db
+        .collection('comments')
+        .orderBy('createdAt', 'desc')
+        .where('screamId', '==', req.params.screamId)
+        .get();
+    })
+    .then(data => {
+      screamData.comments = [];
+      data.forEach(doc => {
+        screamData.comments.push(doc.data());
+      });
+      return res.json(screamData);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: err.code });
+    });
+};
+
+// Comment on a comment
+exports.commentOnScream = (req, res) => {
+  if (req.body.body.trim() === '')
+    return res.status(400).json({ comment: 'Must not be empty' });
+
+  const newComment = {
+    body: req.body.body,
+    createdAt: new Date().toISOString(),
+    screamId: req.params.screamId,
+    userHandle: req.user.handle,
+    userImage: req.user.imageUrl,
+  };
+  // console.log(newComment);
+
+  db.doc(`/screams/${req.params.screamId}`)
+    .get()
+    .then(doc => {
+      if (!doc.exists) {
+        return res.status(404).json({ error: 'Scream not found. Try again!' });
+      }
+      return doc.ref.update({ commentCount: doc.data().commentCount + 1 });
+    })
+    .then(() => {
+      return db.collection('comments').add(newComment);
+    })
+    .then(() => {
+      res.json(newComment);
+    })
+    .catch(err => {
+      console.error(err);
+      res.status(500).json({ error: 'Something went wrong!' });
+    });
+};
--- dev-media-functions/functions/index.js
+++ dev-media-functions/functions/index.js
@@ -2,8 +2,12 @@ const functions = require('firebase-functions');
 const app = require('express')();
 
 const FBAuth = require('./util/fbAuth');
-
-const { getAllScreams, postOneScream } = require('./handlers/screams');
+const {
+  getAllScreams,
+  postOneScream,
+  getScream,
+  commentOnScream
+} = require('./handlers/screams');
 const {
   signup,
   login,
@@ -12,12 +16,32 @@ const {
   getAuthenticatedUser,
 } = require('./handlers/users');
 
+// scream routes
 // get data from firestore || screams route
 app.get('/screams', getAllScreams);
 // create new data and add to firestore
 app.post('/scream', FBAuth, postOneScream);
 
+// get one scream by id route
+app.get('/scream/:screamId', getScream);
+
+// delete one scream by id route
+// app.delete('/scream/:screamId', FBAuth, deleteScream);
+
+// // like a scream route
+// app.get('/scream/:screamId/like', FBAuth, likeScream);
+
+// // unlike scream route
+// app.get('/scream/:screamId/unlike', FBAuth, unlikeScream);
+
+// comment a scream route
+app.post('/scream/:screamId/comment', FBAuth, commentOnScream);
+
 // users routes
+// Sign UP route
+app.post('/signup', signup);
+// login routes
+app.post('/login', login);
 // image upload route
 app.post('/user/image', FBAuth, uploadImage);
 // Add user details route
@@ -25,11 +49,6 @@ app.post('/user', FBAuth, addUserDetails);
 
 // To get Authenticated User
 app.get('/user', FBAuth, getAuthenticatedUser);
-
-// Sign UP route
-app.post('/signup', signup);
-// login routes
-app.post('/login', login);
 
 exports.api = functions.https.onRequest(app);
 
