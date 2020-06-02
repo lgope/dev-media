b7f46bceb8f9db2655d2ea7ecfe582c4d9b3c9d3
lgope <ilakshmangope@gmail.com>
2020-06-02 22:22:30 +0600

    get screams.

--- client/src/App.js
+++ client/src/App.js
@@ -1,29 +1,36 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import './App.css';
+import MuiThemeProvider from '@material-ui/core/styles/MuiThemeProvider';
+import createMuiTheme from '@material-ui/core/styles/createMuiTheme';
 
 // All components
 import Navbar from './components/navbar.component';
+import themeObj from './utils/theme';
 
 // all pages
 import homepage from './pages/homepage.component';
 import login from './pages/login.component';
 import signup from './pages/signup.component';
 
+const theme = createMuiTheme(themeObj);
+
 function App() {
   return (
-    <div className=''>
-      <Router>
-        <Navbar />
-        <div className='container'>
-          <Switch>
-            <Route exact path='/' component={homepage} />
-            <Route exact path='/login' component={login} />
-            <Route exact path='/signup' component={signup} />
-          </Switch>
-        </div>
-      </Router>
-    </div>
+    <MuiThemeProvider theme={theme}>
+      <div className='App'>
+        <Router>
+          <Navbar />
+          <div className='container'>
+            <Switch>
+              <Route exact path='/' component={homepage} />
+              <Route exact path='/login' component={login} />
+              <Route exact path='/signup' component={signup} />
+            </Switch>
+          </div>
+        </Router>
+      </div>
+    </MuiThemeProvider>
   );
 }
 
