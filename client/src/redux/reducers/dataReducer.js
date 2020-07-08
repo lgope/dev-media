c064026ccff1d214cf644ec0a5b9a096afea06a0
lgope <ilakshmangope@gmail.com>
2020-07-08 22:24:45 +0600

    added like and comment button in scream dialog 🚀👍

--- client/src/redux/reducers/dataReducer.js
+++ client/src/redux/reducers/dataReducer.js
@@ -33,9 +33,9 @@ export default function (state = initialState, action) {
         scream => scream.screamId === action.payload.screamId
       );
       state.screams[index] = action.payload;
-      // if (state.scream.screamId === action.payload.screamId) {
-      //   state.scream = action.payload;
-      // }
+      if (state.scream.screamId === action.payload.screamId) {
+        state.scream = action.payload;
+      }
       return {
         ...state,
       };
