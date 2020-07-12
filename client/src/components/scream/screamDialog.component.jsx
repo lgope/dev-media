98368a0f2bc794ddada553ad560b063da632f54e
lgope <ilakshmangope@gmail.com>
2020-07-12 23:46:57 +0600

    added comment form 📝

--- client/src/components/scream/screamDialog.component.jsx
+++ client/src/components/scream/screamDialog.component.jsx
@@ -3,6 +3,7 @@ import withStyles from '@material-ui/core/styles/withStyles';
 import PropTypes from 'prop-types';
 import MyButton from '../../utils/MyButton';
 import LikeButton from '../Like/LikeButton.component';
+import CommentForm from '../Comment/CommentForm.component';
 import Comments from '../Comment/Comments.component';
 import dayjs from 'dayjs';
 import { Link } from 'react-router-dom';
@@ -130,7 +131,7 @@ class ScreamDialog extends Component {
           <span>{commentCount} comments</span>
         </Grid>
         <hr className={classes.visibleSeparator} />
-        {/* <CommentForm screamId={screamId} /> */}
+        <CommentForm screamId={screamId} />
         <Comments comments={comments} />
       </Grid>
     );
